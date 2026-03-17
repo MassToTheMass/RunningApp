1a0b8f2192dcc04dbf15109bd4e18f865da195d0
Mason Christopherson <masonxiv.14@icloud.com>
2026-03-17 11:17:18 -0600

    make a function to create a run table if it doesn't exist

--- parser/databaseCommands.py
+++ parser/databaseCommands.py
@@ -1,8 +1,23 @@
 import sqlite3 as sql
 
-def createTable():
+def createRunTable():
 
-	pass
+	connection = sql.connect("trackData.db")
+	cursor = connection.cursor()
+	cursor.execute('''
+	CREATE TABLE IF NOT EXISTS Runs
+			(id INTEGER PRIMARY KEY AUTOINCREMENT, -- Run ID
+			date TEXT NOT NULL,					   -- Date of the run
+			distance REAL,						   -- Total distance in TODO: add units
+			duration INTEGER,					   -- Duration in seconds
+			avg_pace REAL,						   -- Average pace in TODO: add units
+			elevation_gain REAL					   -- Total elevation gain in TODO: add units
+	)
+	''')
+
+	connection.commit()
+	connection.close()
+
 
 def insertData(track_point):
 
